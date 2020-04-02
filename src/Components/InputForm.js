25fa7a4fa17495184634761ac77f6e75bb2df07e
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-04-01 22:35:00 -0300

    Create InputForm component

---
+++ src/Components/InputForm.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+class InputForm extends Component {
+  /*
+  If the user did not type anything, he/she should not be
+  allowed to submit.
+  */
+  isDisabled = () => {
+    return false
+  }
+
+  render() {
+    return (
+      <div>
+        <form className="input-group">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter your message..."
+          />
+          <div className="input-group-append">
+            <button className="btn submit-button" disabled={this.isDisabled()}>
+              SEND
+            </button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default InputForm
